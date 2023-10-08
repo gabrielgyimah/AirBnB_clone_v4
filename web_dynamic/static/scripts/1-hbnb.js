542c86356f986afa6c583523fd7e7bf2743e12a2
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-10-08 07:53:09 -0700

    Added functionalities to handle the amenity filter selection

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-	const amenity_checkers = $('.popover > ul > li')
+$(document).ready(function () {
+	let amenities = {};
+
+	const amenity_checkers = $('li input[type=checkbox]')
+	amenity_checkers.on('change', function () {
+		if (this.checked) {
+			amenities[$(this).attr("data-name")] = $(this).attr("data-id");
+		} else {
+			delete amenities[$(this).attr("data-name")];
+		}
+		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+	})
 })
